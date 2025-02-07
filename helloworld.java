7cb62d35daf1a09de6ec3313e00416b121084931
Aubin <mugishaaubin0@gmail.com>
2025-02-07 02:03:22 -0500

    added goodbye msg

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,9 @@
 public class helloworld {
 
     public static void main(String[] args) {
-        
+        System.out.println("Here ye: important announcement"); 
         System.out.println("Hello, world!");
+        System.out.println("And finally, goodbye fellas!");
+        
 }
 }
