bcb3e49140f928ce1a9f756974500cd7877ca137
Aubin <mugishaaubin0@gmail.com>
2025-02-14 00:48:42 -0500

    Added feature to display free memory. Closes #2

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,10 @@
-import java.time.LocalDate;
 
 public class helloworld {
 
     public static void main(String[] args) {
         
         System.out.println("Hello, World!");
-        System.out.println("Today's date: " + LocalDate.now());
+        System.out.println("Free memory (bytes): " + Runtime.getRuntime().freeMemory());
 
         
 }
